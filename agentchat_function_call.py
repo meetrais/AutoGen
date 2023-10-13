aee811c2108d3bb2ee7ec2e6a386da7e33bb36cb
meetrais <meetrais@gmail.com>
2023-10-13 13:47:39 -0500

    Task Solving with Provided Tools as Functions

---
+++ agentchat_function_call.py
@@ -0,0 +1,43 @@
+import autogen
+from apikey import apikey
+
+config_list=[{
+    "model":"gpt-4",
+    "api_key": apikey
+}]
+
+llm_config = {
+    "functions": [
+        {
+            "name": "python",
+            "description": "run cell in ipython and return the execution result.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "cell": {
+                        "type": "string",
+                        "description": "Valid Python cell to execute.",
+                    }
+                },
+                "required": ["cell"],
+            },
+        },
+        {
+            "name": "sh",
+            "description": "run a shell script and return the execution result.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "script": {
+                        "type": "string",
+                        "description": "Valid shell script to execute.",
+                    }
+                },
+                "required": ["script"],
+            },
+        },
+    ],
+    "config_list": config_list,
+    "request_timeout": 120,
+}
+
---
+++ tools_as_functions.py
@@ -0,0 +1,76 @@
+import autogen
+from apikey import apikey
+from IPython import get_ipython
+
+config_list=[{
+    "model":"gpt-4",
+    "api_key": apikey
+}]
+
+llm_config = {
+    "functions": [
+        {
+            "name": "python",
+            "description": "run cell in ipython and return the execution result.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "cell": {
+                        "type": "string",
+                        "description": "Valid Python cell to execute.",
+                    }
+                },
+                "required": ["cell"],
+            },
+        },
+        {
+            "name": "sh",
+            "description": "run a shell script and return the execution result.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "script": {
+                        "type": "string",
+                        "description": "Valid shell script to execute.",
+                    }
+                },
+                "required": ["script"],
+            },
+        },
+    ],
+    "config_list": config_list,
+    "request_timeout": 120,
+}
+
+chatbot = autogen.AssistantAgent(
+    name="chatbot",
+    system_message="For coding tasks, only use the functions you have been provided with. Reply TERMINATE when the task is done.",
+    llm_config=llm_config
+    )
+
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    is_termination_msg=lambda x: x.get("content", "") and x.get("content", "").rstrip().endswith("TERMINATE"),
+   human_input_mode="NEVER",
+   max_consecutive_auto_reply=10,
+   code_execution_config={"work_dri": "coding"}
+)
+
+def exec_python(cell):
+    ipython = get_ipython()
+    result = ipython.run_cell(cell)
+    log = str(result.result)
+    if result.error_before_exec is not None:
+        log += f"\n{result.error_before_exec}"
+    if result.error_in_exec is not None:
+        log +=f"\n{result.error_in_exec}"
+
+    return log
+
+def exec_sh(script):
+    return user_proxy.execute_code_blocks([("sh", script)])
+
+user_proxy.initiate_chat(
+    chatbot,
+    message = "Draw two agents chatting with each other with an example dialog. Don't add plt.show().",
+)
